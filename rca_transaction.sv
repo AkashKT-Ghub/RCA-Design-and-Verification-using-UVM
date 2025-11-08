9e17f6da848d3b59ee60e442b96b9fe89f0879b0
Akash K T <ktakash02@gmail.com>
2025-11-09 00:30:22 +0530

    rca_transaction.sv
    Transaction Class of RCA in SV

---
+++ rca_transaction.sv
@@ -0,0 +1,22 @@
+//RCA Transaction
+class adder_transaction extends uvm_sequence_item;
+  
+  
+  `uvm_object_utils_begin(adder_transaction)
+  `uvm_field_int(A,UVM_ALL_ON)
+  `uvm_field_int(B,UVM_ALL_ON)
+  `uvm_field_int(Cin,UVM_ALL_ON)
+  `uvm_field_int(S,UVM_ALL_ON)
+  `uvm_field_int(Cout,UVM_ALL_ON)
+  `uvm_object_utils_end
+  
+    rand bit [7:0] A, B;
+    rand bit Cin;
+    bit [7:0] S;
+    bit Cout;
+  
+    function new(string name = "adder_transaction");
+        super.new(name);
+    endfunction
+  
+endclass
